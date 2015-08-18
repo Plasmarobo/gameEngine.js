972618da2a884f80e89737ba1cf0e874677c2121
Austen Higgins-Cassidy <plasmarobo@gmail.com>
2015-08-18 10:47:51 -0400

    Create engine header
    Sync from Alleynoir

---
+++ engine_header.js
@@ -0,0 +1,25 @@
+var expectedWidth = 1280;
+var expectedHeight = 720;
+var pixelScale = 8; // 8 px to one unit at scale 1 
+var KEYSTATE_UP = 0;
+var KEYSTATE_DOWN = 1;
+var DEBOUNCE_DELAY = 100;
+var KeyCodes =
+{
+  BACKSPACE : 8,
+  TAB       : 9,
+  ENTER     : 13,
+  SHIFT     : 16,
+  CTRL      : 17,
+  ALT       : 18,
+  ESC       : 27,
+  SPACE     : 32,
+  LEFT      : 37,
+  UP        : 38,
+  RIGHT     : 39,
+  DOWN      : 40,
+  A         : 65,
+  D         : 68,
+  S         : 83,
+  W         : 87,
+};
